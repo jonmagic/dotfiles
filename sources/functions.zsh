42d27ddb60def71b472f8d51a229c193e7b08e92
Jonathan Hoyt <jonmagic@gmail.com>
2025-01-30 08:18:35 -0800

    Add functions file with update_software function

---
+++ sources/functions.zsh
@@ -0,0 +1,24 @@
+function homebrew() {
+  brew update && brew outdated && brew upgrade
+}
+
+function update_software() {
+  echo "==> Upgrading antigen plugins"
+  antigen update
+  echo ""
+
+  echo "==> Upgrading gh extensions"
+  gh extension upgrade --all
+  echo ""
+
+  echo "==> Upgrading asdf plugins"
+  asdf plugin-update --all
+  echo ""
+
+  echo "==> Upgrading homebrew"
+  homebrew
+  echo ""
+
+  echo "==> Upgrading Mac App Store apps"
+  mas upgrade
+}
